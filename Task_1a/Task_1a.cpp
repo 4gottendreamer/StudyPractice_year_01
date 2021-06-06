ebcf8b45b676d4bfa9ff50d9bb46de873c2edeee
Дмитрий Калмыков <4gotten.dreamer@gmail.com>
2021-06-06 11:52:30 +0700

    Add project files.

---
+++ Task_1a/Task_1a.cpp
@@ -0,0 +1,15 @@
+// Task_1a.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Вариант 5
+/*
+Задача 1.
+а) Начиная с центра, обойти по спирали все элементы квадратной матрицы n * n,
+располагая их в порядке обхода по и против часовой стрелки.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
