aa5a2f4ebeeaba1afd74438c07f933d22d83b996
Dmitry Kalmykov <4gotten.dreamer@gmail.com>
2021-06-22 17:21:55 +0700

    0

--- Task_1b/Form1.Designer.cs
+++ Task_1b/Form1.Designer.cs
@@ -28,13 +28,88 @@ namespace Task_1b
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.buttonDraw = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.sinX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonDraw
+			// 
+			this.buttonDraw.Location = new System.Drawing.Point(12, 12);
+			this.buttonDraw.Name = "buttonDraw";
+			this.buttonDraw.Size = new System.Drawing.Size(75, 23);
+			this.buttonDraw.TabIndex = 0;
+			this.buttonDraw.Text = "Draw";
+			this.buttonDraw.UseVisualStyleBackColor = true;
+			this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+			this.x,
+			this.sinX});
+			this.dataGridView1.Location = new System.Drawing.Point(618, 41);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowTemplate.Height = 25;
+			this.dataGridView1.Size = new System.Drawing.Size(270, 598);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// x
+			// 
+			this.x.HeaderText = "x";
+			this.x.Name = "x";
+			this.x.ReadOnly = true;
+			// 
+			// sinX
+			// 
+			this.sinX.HeaderText = "sin(x)";
+			this.sinX.Name = "sinX";
+			this.sinX.ReadOnly = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(902, 651);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.buttonDraw);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Sine graph plot";
+			this.Click += new System.EventHandler(this.Form1_Click);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button buttonDraw;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn x;
+		private System.Windows.Forms.DataGridViewTextBoxColumn sinX;
 	}
 }
 
--- Task_1b/Form1.cs
+++ Task_1b/Form1.cs
@@ -12,10 +12,74 @@ namespace Task_1b
 {
 	public partial class Form1 : Form
 	{
+		Graphics g;
+		Pen penBlack;
+		Pen penRed;
+		Point cursor;
 		public Form1()
 		{
 			InitializeComponent();
+			g = pictureBox1.CreateGraphics();
+			penBlack = new Pen(Color.Black, 1);
+			penRed = new Pen(Color.Red, 1);
+			g.DrawLine(penBlack,
+				new Point(0, ClientSize.Height / 2),
+				new Point(ClientSize.Width, ClientSize.Height / 2)
+			);
 		}
 
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			cursor = pictureBox1.PointToClient(Cursor.Position);
+		}
+
+		private void Form1_Click(object sender, EventArgs e)
+		{
+			int radius = 4;
+			g.DrawEllipse(
+				penRed,
+				cursor.X - radius / 2,
+				cursor.Y - radius / 2,
+				radius, radius
+			);
+		}
+
+		private void buttonDraw_Click(object sender, EventArgs e)
+		{
+			g.DrawLine(penBlack,
+				new Point(0, pictureBox1.Height / 2),
+				new Point(pictureBox1.Width, pictureBox1.Height / 2)
+			);
+
+			int radius = 6;
+			int height = pictureBox1.Height;
+			int width = pictureBox1.Width /*- (int)penBlack.Width*/;
+			double h = .1f;
+			double absRange = 1f;
+			double leftBound = -absRange;
+			double rightBound = absRange;
+			double interval = rightBound - leftBound;
+			double phi = leftBound;
+			int steps = (int)(interval / h + 1);
+			int i = 0;
+			Point[] sinePoints = new Point[steps + 1];
+			List<Point> sinPointsList = new List<Point>();
+			while (phi < rightBound + h / 2) {
+				sinePoints[i] = new Point(width / steps * i++, (int)((-height / 2 * Math.Sin(phi)) + height / 2));
+				sinPointsList.Add(sinePoints[^1]);
+				phi += h;
+			}
+			g.DrawCurve(penBlack, sinePoints);
+			foreach (var item in sinePoints) {
+				g.DrawEllipse(penRed, item.X - radius / 2, item.Y - radius / 2, radius, radius);
+			}
+			dataGridView1.Rows.Clear();
+			dataGridView1.Rows.Add(sinePoints.Length);
+			for (int j = 0; j < sinePoints.Length; ++j) {
+				//dataGridView1.Columns.Add();
+				dataGridView1.Rows[j].Cells[0].Value = sinePoints[j].X.ToString();
+				dataGridView1.Rows[j].Cells[1].Value = sinePoints[j].Y.ToString();
+			}
+		}
 	}
 }
